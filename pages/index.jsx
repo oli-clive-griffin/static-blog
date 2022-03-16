177149f627fe8a3c041dfb687fc7e0834a050256
Oli <o.clivegriffin@gmail.com>
2022-03-17 00:11:28 +1300

    redploy?

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 const Home = () => (
   <div className='flex flex-col px-4'>
-    <p>{'Interested in education, psychology, programming, economics, mental models, sci-fi.'}</p>
+    <p>{'Interested in education, psychology, programming, economics, mental models, sci-fi'}</p>
     <div className='h-4' />
   </div>
 )
