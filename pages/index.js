8dc059b76fa6decae45c6d8c9588d6436cd0632d
Oli Clive-Griffin <70186488+oli-clive-griffin@users.noreply.github.com>
2022-03-30 10:44:11 +1300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className='flex flex-col px-4'>
-      <p>{'I\'m a junior full-stack engineer at [halter](https://halter.co.nz/).'}</p>
+      <p>{'I\'m a junior full-stack engineer at '}</p><a href="https://halter.co.nz">halter</a></p>
       <p>{'Interested in education, psychology, weird programming languages, economics, mental models, sci-fi'}</p>
       <div className='h-4' />
     </div>
